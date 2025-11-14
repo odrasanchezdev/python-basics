16d911bc9428d243b79fe4da85d1bb5e65aaae5d
Odra Sanchez <sanchez.00dra@gmail.com>
2025-11-14 02:34:08 -0600

    wip: Modificación del primer código
    - Comentarios en español para apoyar a la comunidad Hispanohablante
    - Mejora en la organización de carpetas
    - Código puesto a prueba
    - Eliminación de códigos previos para su revisión

--- Basics/Patterns/2-RightTriangle.py
+++
@@ -1,5 +0,0 @@
-rows = int(input("Number of lines: "))
-
-# Rows iteration.
-for j in range(1, rows + 1):
-    print(" " * (rows-j) + "*" * j)
--- Basics/Patterns/5-DownTriangle.py
+++
@@ -1,7 +0,0 @@
-rows = int(input("Number of lines: "))
-
-# Rows iteration
-for j in range(rows, 0, -1):
-    print(" " * (rows-j) + "* " * j)
-# Prints a line break after printing all character in the current row.
-print("\r")
